01fa3a29d2cdd83e7fb3ffad21fd8cf09c41dd2e
areoliu <shuangmianshen@gmail.com>
2020-08-07 17:28:51 +0800

    jwt

--- src/main/java/com/example/second/config/ResoureceServerConfig.java
+++ src/main/java/com/example/second/config/ResoureceServerConfig.java
@@ -1,10 +1,16 @@
 package com.example.second.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableResourceServer
@@ -12,20 +18,40 @@ public class ResoureceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String RESOURCE_ID = "order";
 
-//    public void configure(ResourceServerSecurityConfigurer resources) {
-//        // 如果关闭 stateless，则 accessToken 使用时的 session id 会被记录，后续请求不携带 accessToken 也可以正常响应
-//        resources.resourceId(RESOURCE_ID).stateless(true);
-//    }
+    @Autowired
+    private TokenStore tokenStore;
+
+//    @Autowired
+//    private ResourceServerTokenServices tokenServices;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        // 如果关闭 stateless，则 accessToken 使用时的 session id 会被记录，后续请求不携带 accessToken 也可以正常响应
+        resources.resourceId(RESOURCE_ID)
+//                .tokenServices(tokenServices)
+                .tokenStore(tokenStore)
+                .stateless(true);
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/oauth/confirm_access").permitAll()
-                .antMatchers("/**/*.js").permitAll()
-                .antMatchers("/favicon.ico").permitAll()
+                .antMatchers("/**").access("#oauth2.hasScope('all')")
                 .and()
-                .requestMatchers().antMatchers("/order/**").and().authorizeRequests().antMatchers("/order/**").authenticated();
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
+
+
+//    @Bean
+//    public ResourceServerTokenServices tokenServices(){
+//
+//        RemoteTokenServices services = new RemoteTokenServices();
+//        services.setCheckTokenEndpointUrl("http://localhost:9005/oauth/check_token");
+//        services.setClientId("net5ijy");
+//        services.setClientSecret("123456");
+//        return services;
+//    }
 }
 
--- src/test/java/com/example/second/SecondApplicationTests.java
+++ src/test/java/com/example/second/SecondApplicationTests.java
@@ -2,13 +2,39 @@ package com.example.second;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class SecondApplicationTests {
-
+    private static final String PUBLIC_KEY = "public.key";
     @Test
     void contextLoads() {
+        Resource resource = new ClassPathResource(PUBLIC_KEY);
+        String publicKey = null;
+        try {
+            publicKey = inputStream2String(resource.getInputStream());
+            System.out.println(publicKey);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    String inputStream2String(InputStream is) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(is));
+        StringBuffer buffer = new StringBuffer();
+        String line = "";
+        while ((line = in.readLine()) != null) {
+            buffer.append(line);
+        }
+        return buffer.toString();
     }
 
 }
